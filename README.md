ce0f8d075bda213ee52ea6ccb322dfadc0ee8ed5
Gennady Borodin <Gennady.Borodin@quest.com>
2018-11-02 20:24:22 +0300

    Documentation update

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# robotframework-traillibrary
-Robot Framework listener library that provides a simple way to log test results to TestRail test case management system
+# RobotFramework TRailLibrary
+
+## Description
+
+[Robot Framework](http://www.robotframework.org) listener library that provides a simple way to log test results to [TestRail](http://www.gurock.com/testrail/) test case management system
+
+## Installation
+
+```python
+python setup.py install
+```
+
+## Documentation
+
+See library documentation on [GitHub](https://github.com/gennadii-borodin/robotframework-traillibrary/tree/master/docs)
+
+## Library Usage
+
+1. Import TRailLibrary
+
+   ```robot
+   *** Settings ***
+   Library    TRailLibrary    url=http:\\server    user=user@domain.com    api_key=key_here    project=My Project    plan=Test Plan    run_name=MyDailyRun    config={'OS':'Windows', 'Browser':'Chrome'}    prefix=C
+
+   ```
+
+2. Mark Robot Framework tests with tag containing Test Rail case ID. Case IDs can be found in Test Rail UI. They looks like the follofing C# (e.g. C17).
+
+3. If multiple tags specified then all corresponding test cases in Test Rail will receive results.
+
+## Examples
+
+### Single tag example
+
+   ```robot
+   *** Test Cases ***
+   Test With Test Rail tag
+       [Tags]    C1    dummy    owner-johndoe
+       Log    Hello, world!
+   ```
+
+### Multiple tags example
+
+```robot
+   *** Test Cases ***
+   Test With Test Rail tag
+       [Tags]    C1    C2    C45233    dummy    owner-johndoe
+       Log    Hello, world!
+   ```
--- setup.py
+++ setup.py
@@ -1,52 +1,5 @@
 """
-    ``TRailLibrary`` is a Robot Framework listener library that allows to log test execution results
-    to an instanse of Test Rail https://www.gurock.com/testrail Test Case management system
-
-    After the library is imported it starts to listen test execution. When test is finished
-    the libray finds the test tags that contain case ids and sends result to corresponding
-    cases in Test Rail
-
-    ``TRailLibrary`` sends to Test Rail the following information:
-    - test status ``PASS`` or ``FAIL``
-    - ``elapsedtime`` in seconds
-    - ``path`` longname of the test in Robot Framework suite
-    - ``tags`` all test case tags
-    - ``message`` test result message
-
-
-    = Linking test cases =
-
-    Test cases linked throug special Robot Framework tags. Listener finds all tags that match
-    the following regexp
-
-    ``prefix(\d{0,}\d$)``
-
-    where ``prefix`` is value of the appropriate `Importing` argument
-
-    = Configurations =
-    Test Rails configuration, if used, is passed as a dictionary
-    by ``config`` `Importing` argumenet.\n
-    Keys are names of Test Rail configuration groups\n
-    Values are names of configurations
-
-    | ``{'OS':'Windows', 'Browser':'Chrome'}``
-
-
-    = Examples =
-
-    In the following example execution result will be assined to case with id ``C1``
-    in appropriate ``run_name``
-
-    | ***** Test Cases *****
-    | *Test With Test Rail tag*
-    |    [Tags]    C1    dummy    owner-johndoe
-    |    Log    Hello, world!
-
-    If multiple tags specified all matched cases will receive test result
-
-    | [Tags]    C1    C2    C3    dummy    owner-johndoe
-
-    C1, C2 and C3 accordingly
+    Robot Framework listener library setup module
 
 """
 
