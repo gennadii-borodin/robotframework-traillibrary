2e2c73902454635b085754cff148dde7a7da1c5a
Gennady Borodin <Gennady.Borodin@quest.com>
2018-11-12 18:07:47 +0300

    CAse IDs arg added

--- createtestrun.py
+++ createtestrun.py
@@ -23,26 +23,33 @@ def main():
 
     """
 
-    parser = argparse.ArgumentParser(description='Create a Test Run in Test Rails')
+    parser = argparse.ArgumentParser(description='Create a Test Run in Test Rails',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
     parser.add_argument('-user', action='store', dest='tr_user', required=True,
-                        help='TestRail user')
+                        help='TestRail user', metavar='\b')
     parser.add_argument('-url', action='store', dest='tr_url', required=True,
-                        help='TestRail server URL')
+                        help='TestRail server URL', metavar='\b')
     parser.add_argument('-key', action='store', dest='tr_key', required=True,
-                        help='TestRail API key')
+                        help='TestRail API key', metavar='\b')
     parser.add_argument('-project', action='store', dest='project', required=True,
-                        help='TestRail project')
+                        help='TestRail project', metavar='\b')
     parser.add_argument('-suite', action='store', dest='test_suite', default='Master',
-                        help='TestRail test suite. ''Master'' if ommited')
+                        help='TestRail test suite. ''Master'' if ommited', metavar='\b')
     parser.add_argument('-plan', action='store', dest='test_plan', required=True,
-                        help='TestRail test plan')
+                        help='TestRail test plan', metavar='\b')
     parser.add_argument('-run', action='store', dest='test_run', required=True,
-                        help='The name of TestRail test run that will be created')
+                        help='The name of TestRail test run that will be created',
+                        metavar='\b')
     parser.add_argument('-d', action='store', dest='run_description', default=None,
-                        help='TestRail test run dscription')
-    parser.add_argument('-configs', action='store', dest='configset', required=True,
-                        help='List of dictionaries containing TestRail configurations sets')
-    
+                        help='TestRail test run dscription', metavar='\b')
+    parser.add_argument('-configs', action='store', dest='configset', required=False,
+                        default='[]', help='Optional list of dictionaries containing '
+                        'TestRail configurations sets', metavar='\b')
+    parser.add_argument('-caseids', action='store', dest='caseids', required=False,
+                        default=None, help='Optional list of case IDs to be included'
+                        'to the test run', metavar='\b')
+
     args = parser.parse_args()
     pprint.pprint("")
     pprint.pprint("")
@@ -53,8 +60,16 @@ def main():
     test_rail = TestRailAdaptor(args.tr_url, args.tr_user, args.tr_key)
     test_rail.select_project(args.project)
     test_rail.select_plan(args.test_plan)
+
+    include_all = True
+
+    if args.caseids:
+        include_all = False
+
     result = test_rail.create_run(args.test_run, args.run_description,
-                                  args.test_suite, ast.literal_eval(args.configset))
+                                  args.test_suite, ast.literal_eval(args.configset),
+                                  include_all=include_all,
+                                  case_ids=ast.literal_eval(args.caseids))
 
     pprint.pprint("")
     pprint.pprint("")
